f3d979b90586275a6c6800cbe6e1b11f3924358b
m.peters <snakeyes721@outlook.com>
2019-10-08 08:27:44 -0700

    bam

---
+++ bamazonCustomer.js
@@ -0,0 +1,16 @@
+let mysql = require("mysql");
+
+let connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 6969,
+
+  user: "sozeb",
+
+  password: "money",
+  database: "bamazonDB"
+});
+
+connection.connect(function(error) {
+  if (error) throw error;
+})
