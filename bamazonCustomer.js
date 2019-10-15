4c04a47f48954b02c9f4daea27c0cc2fd70f1717
m.peters <snakeyes721@outlook.com>
2019-10-14 20:50:46 -0700

    pseudo arranged

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -61,28 +61,30 @@ function productSearch() {
               type: "number",
               message: "How many would you like to purchase?"
             })
-            .then(function (quantityChoice){
+            //check if there is enough in stock
+            .then(function (quantityChoice) {
               let quantity = quantityChoice.quantity;
-                if (quantity > res[0].stock_quantity) {
-                  console.log("------------------------------------------------------------------------------------------------------------------------");
-                  console.log("Sorry, we only have " + res[0].stock_quantity + " on hand. Please reselect your item at a lower quantity.");
-                  console.log("");
-                  productSearch();
-                }
-                else {
-                  console.log("------------------------------------------------------------------------------------------------------------------------");
-                  console.log("You have selected " +  quantity + " " + res[0].product_name + "(s) for a price of $" + res[0].price + " each.");
-                  console.log("Your total comes to $" +  quantity*res[0].price);
-                }
-              })
+              if (quantity > res[0].stock_quantity) {
+                //insufficient alert 
+
+                console.log("------------------------------------------------------------------------------------------------------------------------");
+                console.log("Sorry, we only have " + res[0].stock_quantity + " on hand. Please reselect your item at a lower quantity.");
+                console.log("");
+                productSearch();
+                //or
+              }
+              else {
+                //fulfill customer order
+                console.log("------------------------------------------------------------------------------------------------------------------------");
+                console.log("You have selected " + quantity + " " + res[0].product_name + "(s) for a price of $" + res[0].price + " each.");
+                //show cost of purchase
+                console.log("Your total comes to $" + quantity * res[0].price);
+              }
+            })
 
 
-          //check if there is enough in stock
-          //insufficient alert 
-          //or
-          //fulfill customer order
           //update inventory count
-          //show cost of purchase
+          
         }
       })
     })
